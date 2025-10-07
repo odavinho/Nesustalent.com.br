3494153b4e1221d375f4e19c644dd9028436ce13
Octávio Martins <odavinho@gmail.com>
2025-10-07 00:55:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-873222438-552ba",
+  "appId": "1:616449591627:web:80ff48c010c721306500ce",
+  "apiKey": "AIzaSyBv-Xb7WwmFOGVZ3TiKmAR0YHWz8ldK_lc",
+  "authDomain": "studio-873222438-552ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "616449591627"
+};
